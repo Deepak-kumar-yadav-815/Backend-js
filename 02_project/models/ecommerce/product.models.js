d687f7359fb34f83ccd07821ef90d03397ec683e
Deepak kumar yadav <deepakkyadav815@gmail.com>
2026-05-25 21:11:50 +0530

    models designing

---
+++ 02_project/models/ecommerce/product.models.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const ProdutSchema  = new mongoose.Schema(
+    {
+        description :{
+            required : true,
+            type: String
+        },
+       name : {
+            type : String,
+            required : true,
+       },
+       productImg : {
+        type : String,
+       },
+       price :{
+        type : Number,
+        default :0,
+       },
+       stock :{
+        default :0,
+        type:Number
+       },
+       category:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref:'Category',
+        required:true
+       },
+       owner : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User"
+       }
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const Product = mongoose.model('Product',ProdutSchema);
